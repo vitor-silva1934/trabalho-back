b97572f4edcb571898a476a2bd34ae958c769c47
unknown <vitor_hugo1934@hotmail.com>
2024-10-15 20:03:25 -0300

    criação de pagina HTML

--- .venv/setup/urls.py
+++ .venv/setup/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from projeto.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+
+    
 ]
